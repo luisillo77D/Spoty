837a4fb481b2fd8e69b392e91a2ef7932bbbc5c5
luisillo77D <luisillo77mario@gmail.com>
2024-05-05 23:04:28 -0700

    feat: Update generos.js to obtain access token for Spotify API
    The generos.js file was updated to include a function called "obtenerTokenAcceso" that generates an access token for the Spotify API. This function makes a POST request to the Spotify token endpoint with the client ID and client secret as headers and the grant type as the request body. The access token is then stored in the ACCESS_TOKEN variable. This change was made to ensure that the API requests in the file are authenticated and authorized.

--- js/generos.js
+++ js/generos.js
@@ -1,6 +1,25 @@
-ACCESS_TOKEN = localStorage.getItem('access_token');
+ACCESS_TOKEN 
 URLI ="https://api.spotify.com/v1/recommendations/available-genre-seeds"
+const CLIENT_ID = "5f49bb2bd6d24e96a3cbfdeffb9071f8";
+const CLIENT_SECRET = "b579f422e44f4b289ec5cecb40e1d208";
 let generos = {};
+
+//funcion para generar el token de acceso
+async function obtenerTokenAcceso() {
+  const respuesta = await fetch("https://accounts.spotify.com/api/token", {
+    method: "POST",
+    headers: {
+      Authorization: "Basic " + btoa(CLIENT_ID + ":" + CLIENT_SECRET),
+      "Content-Type": "application/x-www-form-urlencoded",
+    },
+    body: "grant_type=client_credentials",
+  });
+  const datos = await respuesta.json();
+  console.log(datos);
+  return datos.access_token;
+}
+
+ACCESS_TOKEN = await obtenerTokenAcceso();
 
 const getGeneros = async () => {
   try {
