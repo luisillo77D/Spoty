e98554c9a104a22654ea61e78b7db71191f656ef
luisillo77D <luisillo77mario@gmail.com>
2024-05-11 13:40:24 -0700

    refactor: Move Spotify authentication logic from callback.js to toptracks.js
    The getToken function in callback.js was removed and its functionality was moved to the getToken function in toptracks.js. This change ensures that the access token is obtained before making API requests in toptracks.js, improving the authentication process for Spotify API calls.

--- js/callback.js
+++ js/callback.js
@@ -1,11 +1,41 @@
 
 const CLIENT_ID = "5f49bb2bd6d24e96a3cbfdeffb9071f8";
 const CLIENT_SECRET = "b579f422e44f4b289ec5cecb40e1d208";
+const REDIRECT_URI = "http://localhost:3000/callback.html";
+
+  // Inicializa la autenticación de Spotify al hacer clic en el botón
+const getToken = async (code) => {
+    // stored in the previous step
+    try {
+      const url = "https://accounts.spotify.com/api/token";
+      const payload = {
+        method: "POST",
+        url: url,
+        headers: {
+          "Content-Type": "application/x-www-form-urlencoded",
+          Authorization: `Basic ${btoa(`${CLIENT_ID}:${CLIENT_SECRET}`)}`,
+        },
+        body: new URLSearchParams({
+          grant_type: "authorization_code",
+          code,
+          redirect_uri: REDIRECT_URI,
+        }),
+      };
+      
+      const body = await fetch(url, payload);
+      const response = await body.json();
+      console.log(response);
+      localStorage.setItem("access_token", response.access_token);
+    } catch (error) {
+      console.error(error);
+    }
+  };
 
   async function callback() {
     const url = new URL(window.location.href);
     const code = url.searchParams.get("code");
     localStorage.setItem("code", code);
+    await getToken(code);
     window.location.href = "topTracks.html";
   }
 
