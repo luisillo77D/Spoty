5218a27093a7b807de16b922b4bc04ae9f94f038
luisillo77D <luisillo77mario@gmail.com>
2024-05-11 12:06:04 -0700

    feat: Add Bootstrap 5.3.3 as a dependency and import custom.scss
    This commit adds Bootstrap 5.3.3 as a dependency in the package.json file and imports the custom.scss file in the scss folder. The custom.scss file defines a new variable, $success, with a color value, and imports the Bootstrap SCSS files. This change is made to enhance the styling and layout of the application.

--- js/generos.js
+++ js/generos.js
@@ -2,50 +2,54 @@ let ACCESS_TOKEN
 URLI ="https://api.spotify.com/v1/recommendations/available-genre-seeds"
 const CLIENT_ID = "5f49bb2bd6d24e96a3cbfdeffb9071f8";
 const CLIENT_SECRET = "b579f422e44f4b289ec5cecb40e1d208";
+const btnGeneros = document.getElementById("btn-generos");
+//borramos el local storage
+localStorage.removeItem("genres");
+localStorage.removeItem("artistas");
 let generos = {
   pop: {
       genres: "pop",
-      artistas: ["06HL4z0CvFAxyc27GXpf02", "66CXWjxzNUsdJxJ2JdwvnR", "6eUKZXaKkcviH0Ku9w2n3V", "6qqNVTkY8uBg9cP3Jd7DAH", "1uNFoZAHBGtllmzznpCI3s"],
+      artistas: ["06HL4z0CvFAxyc27GXpf02", "66CXWjxzNUsdJxJ2JdwvnR"],
       selected: false
   },
   hipHop: {
       genres: "hip-hop",
-      artistas: ["3TVXtAsR1Inumwj472S9r4", "5K4W6rqBFWDnAN6FQUkS6x", "0Y5tJX1MQlPlqiwlOH1tJY", "246dkjvS1zLTtiykXe5h60", "4kYSro6naA4h99UJvo89HB"],
+      artistas: ["3TVXtAsR1Inumwj472S9r4", "5K4W6rqBFWDnAN6FQUkS6x"],
       selected: false
   },
   rock: {
       genres: "rock",
-      artistas: ["3WrFJ7ztbogyGnTHbHJFl2", "1dfeR4HaWDbWqFHLkxsg1d", "36QJpDe2go2KgaRleHCDTp", "0k17h0D3J5VfsdmQ1iZtE9", "711MCceyCBcFnzjGY4Q7Un"],
+      artistas: ["3WrFJ7ztbogyGnTHbHJFl2", "1dfeR4HaWDbWqFHLkxsg1d"],
       selected: false
   },
   reggaeton: {
       genres: "reggaeton",
-      artistas: ["4VMYDCV2IEDYJArk749S6m", "4q3ewBCX7sLwd24euuV69X", "6nVcHLIgY5pE2YCl8ubca1", "2LRoIwlKmHjgvigdNGBHNo", "2R21vXR83lH98kGeO99Y66"],
+      artistas: ["4VMYDCV2IEDYJArk749S6m", "4q3ewBCX7sLwd24euuV69X"],
       selected: false
   },
   rap: {
       genres: "rap",
-      artistas: ["7dGJo4pcD2V6oG8kP0tJRR", "3nFkdlSjzX9mRTtwJOzDYB", "2YZyLoL8N0Wb9xBt1NhZWg", "20qISvAhX20dpIbOOzGK3q", "5me0Irg2ANcsgc93uaYrpb"],
+      artistas: ["7dGJo4pcD2V6oG8kP0tJRR", "3nFkdlSjzX9mRTtwJOzDYB"],
       selected: false
   },
   indie: {
       genres: "indie",
-      artistas: ["7Ln80lUS6He07XvHI8qqHH", "0epOFNiUfyON9EYx7Tpr6V", "5INjqkS1o8h1imAzPqGZBb", "5BvJzeQpmsdsFp4HGUYUEx", "4Z8W4fKeB5YxbusRsdQVPb"],
+      artistas: ["7Ln80lUS6He07XvHI8qqHH", "0epOFNiUfyON9EYx7Tpr6V"],
       selected: false
   },
   corridosTumbados: {
       genres: "latin,latino",
-      artistas: ["0elWFr7TW8piilVRYJUe4P", "0ys2OFYzWYB5hRDLCsBqxt", "7Gi6gjaWy3DxyilpF1a8Is", "3l9G1G9MxH6DaRhwLklaf5", "12GqGscKJx3aE4t07u7eVZ"],
+      artistas: ["0elWFr7TW8piilVRYJUe4P", "0ys2OFYzWYB5hRDLCsBqxt"],
       selected: false
   },
   latinpop: {
       genres: "latin,latino,pop",
-      artistas: ["1jtvmXiemNFkPO11NMdjfu", "4boI7bJtmB1L3b1cuL75Zr", "31VFEohvhOUKrtAONEBhMG", "1b7AEdUSudOQoZF5ebUxCL", "6XTGKOV9jceQ6f67lnhpbF"],
+      artistas: ["1jtvmXiemNFkPO11NMdjfu", "4boI7bJtmB1L3b1cuL75Zr"],
       selected: false
   },
   kpop: {
       genres: "k-pop",
-      artistas: ["3Nrfpe0tUJi4K4DXYWgMUX", "7n2Ycct7Beij7Dj7meI4X0", "2KC9Qb60EaY0kW4eH68vr3", "41MozSoPIsD1dJM0CLPjZF", "2AfmfGFbe0A0WsTYm0SDTx"],
+      artistas: ["3Nrfpe0tUJi4K4DXYWgMUX", "7n2Ycct7Beij7Dj7meI4X0"],
       selected: false
   },
   
@@ -63,20 +67,27 @@ async function obtenerTokenAcceso() {
   });
   const datos = await respuesta.json();
   console.log(datos);
+  localStorage.setItem("token", datos.access_token);
   return datos.access_token;
 }
 
-ACCESS_TOKEN = obtenerTokenAcceso();
+async function obtenerToken() {
+  ACCESS_TOKEN = await obtenerTokenAcceso();
+}
+
+obtenerToken();
 
 //funcion para obtener la url de la imagen del artista
 async function obtenerImagenArtista(id) {
   try {
+    
     const respuesta = await fetch(`https://api.spotify.com/v1/artists/${id}`, {
     headers: {
       Authorization: "Bearer " +  ACCESS_TOKEN,
     },
   });
   const datos = await respuesta.json();
+  console.log(datos.name);
   return datos.images[0].url;
   } catch (error) {
     console.log(error); 
@@ -93,13 +104,14 @@ async function mostrarGeneros() {
     //recorremos cada cada arista del genero y obtenemos la url de la imagen
     let imagen =[]
     for (const artista of generos[genero].artistas) {
+      console.log(artista);
       imagen.push(await obtenerImagenArtista(artista));
     }
-    
+  
     generoHTML.innerHTML = `
-    <div class="card col-10">
-      <h2>${genero}</h2>
-      <div class="artistas">
+    <div class="card col-10 bg-success-subtle">
+      <h2 class="p-2">${genero}</h2>
+      <div class="artistas p-2">
         ${imagen.map((img) => `<img src="${img}" alt="imagen artista" width=75px height=75px style="object-fit: cover;" class="rounded-circle">`).join("")}
         </div>
         <button class="btn btn-primary mt-3 select">Seleccionar</button>
@@ -109,9 +121,11 @@ async function mostrarGeneros() {
     generoHTML.querySelector(".select").addEventListener("click", () => {
       generos[genero].selected = !generos[genero].selected;
       if (generos[genero].selected) {
-        generoHTML.querySelector(".card").classList.add("border-primary", "border-3", );
+        generoHTML.querySelector(".card").classList.add("bg-success" );
+        generoHTML.querySelector(".card").classList.remove("bg-success-subtle");
       } else {
-        generoHTML.querySelector(".card").classList.remove("border-primary", "border-3");
+        generoHTML.querySelector(".card").classList.remove("bg-success");
+        generoHTML.querySelector(".card").classList.add("bg-success-subtle");
       }
     });
     
@@ -119,6 +133,22 @@ async function mostrarGeneros() {
   }
 }
 
-//ejecutar la funcion mostrarGeneros con await para esperar a que se resuelva la promesa
 mostrarGeneros();
+
+btnGeneros.addEventListener("click", () => {
+  console.log(generos);
+  //agregamos solo los generos seleccionados al local storage
+  let generosSeleccionados = [];
+  let artistaSeleccionados = [];
+  for (const genero in generos) {
+    if (generos[genero].selected) {
+      generosSeleccionados.push(generos[genero].genres);
+      artistaSeleccionados.push(generos[genero].artistas);
+    }
+  }
+  localStorage.setItem("genres", JSON.stringify(generosSeleccionados));
+  localStorage.setItem("artistas", JSON.stringify(artistaSeleccionados));
+  window.location.href = "canciones.html";
+  
+});
 
