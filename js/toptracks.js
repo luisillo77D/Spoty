7cf02f9a1723d02877348b1577a371873259a3ec
luisillo77D <luisillo77mario@gmail.com>
2024-05-11 13:25:43 -0700

    feat: Update callback.js and toptracks.js to improve Spotify authentication process
    The getToken function in callback.js was removed and its functionality was moved to the getToken function in toptracks.js. This change ensures that the access token is obtained before making API requests in toptracks.js, improving the authentication process for Spotify API calls.

--- js/toptracks.js
+++ js/toptracks.js
@@ -1,8 +1,38 @@
 const access_token = localStorage.getItem("access_token");
 const endpoint = "https://api.spotify.com/v1/me/top/tracks";
 const type = 'tracks'
+
+// Inicializa la autenticación de Spotify al hacer clic en el botón
+const getToken = async () => {
+    // stored in the previous step
+    try {
+      const url = "https://accounts.spotify.com/api/token";
+      const payload = {
+        method: "POST",
+        url: url,
+        headers: {
+          "Content-Type": "application/x-www-form-urlencoded",
+          Authorization: `Basic ${btoa(`${CLIENT_ID}:${CLIENT_SECRET}`)}`,
+        },
+        body: new URLSearchParams({
+          grant_type: "authorization_code",
+          code: localStorage.getItem("code"),
+          redirect_uri: REDIRECT_URI,
+        }),
+      };
+      
+      const body = await fetch(url, payload);
+      const response = await body.json();
+      console.log(response);
+      localStorage.setItem("access_token", response.access_token);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
 async function obtenerTopTracks() {
     try {
+        await getToken();
         const respuesta = await fetch(`${endpoint}?limit=10`, {
         method: "GET",
         headers: {
