cb92dd284dff5a854e07ddac8572f6e4244bf69d
luisillo77D <luisillo77mario@gmail.com>
2024-05-11 13:12:52 -0700

    feat: Update auth.js to fix redirect URI for Spotify authentication
    The REDIRECT_URI constant in the auth.js file was updated from "http://localhost:3000/" to "http://localhost:3000/callback.html". This change ensures that the authentication process redirects to the correct URL after successful authentication.

--- js/auth.js
+++ js/auth.js
@@ -2,7 +2,7 @@ const CLIENT_ID = "5f49bb2bd6d24e96a3cbfdeffb9071f8";
 const CLIENT_SECRET = "b579f422e44f4b289ec5cecb40e1d208";
 
 // URL de redireccionamiento después de la autenticación
-const REDIRECT_URI = "http://localhost:3000/";
+const REDIRECT_URI = "http://localhost:3000/callback.html";
 
 console.log(localStorage.getItem("code"));
 console.log(localStorage.getItem("access_token"));
@@ -13,39 +13,10 @@ const AUTHORIZATION_URL = `https://accounts.spotify.com/authorize?client_id=${CL
 async function iniciarAutenticacionSpotify() {
   // redirect to the URL in new tab to start the auth process
   window.open(AUTHORIZATION_URL, "_self");
-  const urlParams = new URLSearchParams(window.location.search);
-  let code = urlParams.get("code");
-  window.localStorage.setItem("code", code);
-  await getToken(code);
+
 }
 
-// Inicializa la autenticación de Spotify al hacer clic en el botón
-const getToken = async (code) => {
-  // stored in the previous step
-  try {
-    const url = "https://accounts.spotify.com/api/token";
-    const payload = {
-      method: "POST",
-      url: url,
-      headers: {
-        "Content-Type": "application/x-www-form-urlencoded",
-        Authorization: `Basic ${btoa(`${CLIENT_ID}:${CLIENT_SECRET}`)}`,
-      },
-      body: new URLSearchParams({
-        grant_type: "authorization_code",
-        code,
-        redirect_uri: REDIRECT_URI,
-      }),
-    };
-    
-    const body = await fetch(url, payload);
-    const response = await body.json();
-    console.log(response);
-    localStorage.setItem("access_token", response.access_token);
-  } catch (error) {
-    console.error(error);
-  }
-};
+
 
 // Asocia la función de inicio de autenticación al botón de clic
 const botonAutenticacion = document.getElementById("boton-spotify");
@@ -55,9 +26,10 @@ const botonGenerar= document.getElementById("generate");
 
 //evento listener para el boton de generar, para que nos redirija a generos.html en caso de que ya tengamos el token si no iniciamos la autenticación
 botonGenerar.addEventListener("click",async ()=>{
-  if(localStorage.getItem("access_token")!=undefined || localStorage.getItem("code")!=null){
-    window.location.href = "topTracks.html";
+  if(localStorage.getItem("access_token")!='undefined' || localStorage.getItem("code")!==null){
+    await iniciarAutenticacionSpotify();
     console.log("ya tienes el token");
+    window.location.href = "topTracks.html";
   }else{
     await iniciarAutenticacionSpotify();
     window.location.href = "topTracks.html";
